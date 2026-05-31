d30b7239e0419d84f3c51e12958150ddca34994e
baamelyoussef <imbaamelyoussef@gmail.com>
2026-05-31 21:13:28 +0100

    wip: update content

--- src/Application/UI/components/LoadingScreen.tsx
+++ src/Application/UI/components/LoadingScreen.tsx
@@ -146,19 +146,25 @@ const LoadingScreen: React.FC<LoadingProps> = () => {
                         style={styles.header}
                         className="loading-screen-header"
                     >
+                        <p>Inspired by henryheffernan.com</p>
+                    
+                    </div>
+                    <div
+                        style={styles.header}
+                        className="loading-screen-header"
+                    >
                         <div style={styles.logoContainer}>
                             <div>
+                                
                                 <p style={styles.green}>
-                                    <b>Heffernan,</b>{' '}
-                                </p>
-                                <p style={styles.green}>
-                                    <b>Henry Inc.</b>
+                                    <b>YBaamel Inc.</b>
                                 </p>
                             </div>
                         </div>
                         <div style={styles.headerInfo}>
-                            <p>Released: 01/13/2000</p>
-                            <p>HHBIOS (C)2000 Heffernan Henry Inc.,</p>
+                            <p>Released: 22/05/2000</p>
+                            <p>YBBIOS (C)2000</p>
+                            
                         </div>
                     </div>
                     <div style={styles.body} className="loading-screen-body">
@@ -195,7 +201,7 @@ const LoadingScreen: React.FC<LoadingProps> = () => {
                             <p>
                                 All Content Loaded, launching{' '}
                                 <b style={styles.green}>
-                                    'Henry Heffernan Portfolio Showcase'
+                                    'Youssef Baamel Immersive Portfolio'
                                 </b>{' '}
                                 V1.0
                             </p>
@@ -227,7 +233,7 @@ const LoadingScreen: React.FC<LoadingProps> = () => {
                     <p>But do enjoy what I have done so far :)</p>
                     <div style={styles.spacer} />
                     <div style={styles.spacer} /> */}
-                    <p>Henry Heffernan Portfolio Showcase 2022</p>
+                    <p>Youssef Baamel Immersive Portfolio 2026</p>
                     {mobileWarning && (
                         <>
                             <br />
